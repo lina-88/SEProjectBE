75d7698eae827e52158ae46e2be695d6044867e1
Lina Walid <lina.walid8@gmail.com>
2021-09-07 18:40:07 +0200

    initial commit

---
+++ Data/DemoContext.cs
@@ -0,0 +1,37 @@
+﻿using SEProjectBE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEProjectBE.Data
+{
+    public partial class DemoContext : DbContext
+    {
+
+        
+        public DemoContext()
+        {
+             
+        }
+        public DemoContext(DbContextOptions<DemoContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+      
+       
+        
+
+
+    }
+}
