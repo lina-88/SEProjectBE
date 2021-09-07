a395f86e19874f2a85c5ec5cabf5fefafd5778b7
Lina Walid <lina.walid8@gmail.com>
2021-09-07 21:06:41 +0200

    added jsonsoft

--- Startup.cs
+++ Startup.cs
@@ -30,11 +30,19 @@ namespace SEProjectBE
             {
                 c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         });
-           
+
            
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddDbContext<DemoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMvc(Configuration =>
+            {
+                Configuration.EnableEndpointRouting = false;
+            })
+               .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+               .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
